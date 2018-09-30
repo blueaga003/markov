206c8c9ced294b282d97ad664f06986762b4ca83
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 17:34:47 -0700

    Work in progress

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
-
+import pdb
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -13,7 +13,7 @@ def open_and_read_file(file_path):
     # your code goes here
     text_source = open(file_path)
     file_as_string = text_source.read()
-    return file_as_string	
+    return file_as_string   
 
 def make_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
@@ -39,12 +39,59 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
+    bigrams_with_following_text = {}
+    words = text_string.split()
+    bigram_values = []
+    word_index = 0
 
-    chains = {}
+    print(words)
+    for word_index in range(len(words) - 1):
+        while word_index < len(words) - 2:
+            # pdb.set_trace()
+            #bigram_values.append(words[word_index + 2])
+            diction_keys = (words[word_index], words[word_index + 1])
+            bigrams_with_following_text[diction_keys] += [words[word_index + 2]]
+            # bigrams_with_following_text[(words[word_index], words[word_index + 1])] += [words[word_index + 2]]
+            
 
-    # your code goes here
+            #if word1 and word2 == key, append word3 to key list
+            word_index += 1
+    print(bigrams_with_following_text)
 
-    return chains
+    # for key, value in bigrams_with_following_text.items():
+    #     print(key, value)
+    #     bigrams_with_following_text[key].append("ba")
+       
+
+            
+            
+    #print(bigrams_with_following_text.keys()) 
+        #pdb.set_trace()
+    
+    #print(bigrams_with_following_text)
+       
+
+
+
+
+
+    # chains = {}
+    # chain_keyes = []
+    # # your code goes here
+    # words = text_string.split()
+    # for word_index in range(len(words)-1):
+    #     pdb.set_trace()
+    #     #chain_keyes.append((words[word_index], words[word_index + 1]))
+    #     words_keys = chains.keys()
+    #     chains.get(chain_keyes,0)
+    #     # chains.get((words[word_index], words[word_index + 1]), words[word_index + 2])
+
+    #     print(chains)
+    #     # print("This is a set of two: {} {}".format(words[word_index],words[word_index+1]))
+    #     print(chain_keyes)
+    # return chains
+
+
 
 
 def make_text(chains):
