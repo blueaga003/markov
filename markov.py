8b214cfac5bcd7e017208b708d372ebcd5e18961
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-04 20:52:23 -0700

    Variable number of ngrams

--- markov.py
+++ markov.py
@@ -2,6 +2,7 @@
 
 from random import choice
 import sys
+import pdb
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -40,22 +41,22 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
-    bigrams_with_following_text = {}
+    grams_with_following_text = {}
  
     words = text_string.split()
-    bigram_values = []
+    gram_values = []
     word_index = 0
-
    
     # minus two because index out of range for later use
-    for word_index in range(len(words) - 2):
-        word_pairs = tuple((words[word_index], words[word_index + 1]))
+    for word_index in range(len(words) - ngram_value):
+        #pdb.set_trace()
+        word_combinations = tuple((words[word_index:word_index + ngram_value]))
         
-        value = bigrams_with_following_text.get(word_pairs, [])
-        value.append(words[word_index + 2])
-        bigrams_with_following_text[word_pairs] = value 
+        value = grams_with_following_text.get(word_combinations, [])
+        value.append(words[word_index + (ngram_value)])
+        grams_with_following_text[word_combinations] = value 
     
-    return bigrams_with_following_text
+    return grams_with_following_text
 
 
 def make_text(chains):
@@ -72,25 +73,37 @@ def make_text(chains):
 
 
     for word in chains:
-        words.append(word[1])
+       # print("this is word: {}".format(word))
+        words.extend(word[1:])
+       # print("this is the words list: {}".format(words))
         choice(chains[word])
-        word_pairs = (word[1], choice(chains[word]))
-        if word_pairs in chains.keys():
-            chains[word_pairs]
+        # pdb.set_trace()
+        word_combinations = ()
+        for entry in words[-ngram_value+1:]:
+            word_combinations += tuple([entry])
+            # print("the entry is: {}".format(entry))
+        word_combinations += tuple([choice(chains[word])])
+
+        # word_combinations = (word[1:] + tuple(choice(chains[word])))
+        # print("this is word combinations {}".format(word_combinations))
+        if word_combinations in chains.keys():
+            chains[word_combinations]
         else:
             return " ".join(words)
 
-if len(sys.argv) != 2:
-	print("<USAGE ERROR> python3 markov.py text_input")
-	sys.exit()
+if len(sys.argv) != 3:
+    print("<USAGE ERROR> python3 markov.py text_input number_of_ngrams")
+    sys.exit()
 else:
-	input_path = sys.argv[1]
+    input_path = sys.argv[1]
+    ngram_value = int(sys.argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
 chains_in_dictionary_format = make_chains(input_text)
+print(chains_in_dictionary_format)
 make_text(chains_in_dictionary_format)
 # Produce random text
 random_text = make_text(chains_in_dictionary_format)
